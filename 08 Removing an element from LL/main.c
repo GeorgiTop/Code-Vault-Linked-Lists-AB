86e6a480217183de1bd8890e17b5a7b78bc1e11e
GeorgiTop <88086563+GeorgiTop@users.noreply.github.com>
2022-11-08 10:07:18 +0200

    Added Removing an element from a LL

---
+++ 08 Removing an element from LL/main.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node
+{
+    int x;
+    struct Node *next;
+} Node;
+
+void deallocate(Node **root)
+{
+    Node *curr = *root;
+    while (curr != NULL)
+    {
+        Node *aux = curr;
+        curr = curr->next;
+        free(aux);
+    }
+    *root = NULL;
+}
+
+void insert_end(Node **root, int value)
+{
+    Node *new_node = malloc(sizeof(Node));
+    if (new_node == NULL)
+    {
+        exit(1);
+    }
+    new_node->next = NULL;
+    new_node->x = value;
+
+    if (*root == NULL)
+    {
+        *root = new_node;
+        return;
+    }
+
+    Node *curr = *root;
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = new_node;
+}
+
+void insert_beginning(Node **root, int value)
+{
+    Node *new_node = malloc(sizeof(int));
+    if (new_node == NULL)
+    {
+        exit(3);
+    }
+
+    new_node->x = value;
+    new_node->next = *root;
+
+    *root = new_node;
+}
+
+void insert_after(Node *node, int value)
+{
+    Node *new_node = malloc(sizeof(Node));
+    if (new_node == NULL)
+    {
+        exit(1);
+    }
+
+    new_node->x = value;
+    new_node->next = node->next;
+    node->next = new_node;
+}
+
+void insert_sorted(Node **root, int value)
+{
+    if (*root == NULL || (**root).x >= value)
+    {
+        insert_beginning(root, value);
+        return;
+    }
+
+    Node *curr = *root;
+    while (curr->next != NULL)
+    {
+        if (curr->next->x >= value)
+        {
+            insert_after(curr, value);
+            return;
+        }
+        curr = curr->next;
+    }
+
+    insert_after(curr, value);
+}
+
+void remove_element(Node **root, int value)
+{
+    if (*root == NULL)
+    {
+        return;
+    }
+
+    if ((*root)->x == value) {
+        Node* to_remove = *root;
+        *root = (*root)->next;
+        free(to_remove);
+        return;
+    }
+
+    for(Node* curr = *root; curr->next != NULL; curr = curr->next) {
+        if (curr->next->x == value) {
+            Node* to_remove = curr->next;
+            curr->next = curr->next->next;
+            free(to_remove);
+            return;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    Node *root = NULL;
+
+    insert_sorted(&root, 1);
+    insert_sorted(&root, 3);
+    insert_sorted(&root, 6);
+
+    for (Node *curr = root; curr != NULL; curr = curr->next)
+    {
+        printf("%d\n", curr->x);
+    }
+
+    remove_element(&root, 3);
+
+    printf("\n");
+    for (Node *curr = root; curr != NULL; curr = curr->next)
+    {
+        printf("%d\n", curr->x);
+    }
+
+    remove_element(&root, 1);
+
+    printf("\n");
+    for (Node *curr = root; curr != NULL; curr = curr->next)
+    {
+        printf("%d\n", curr->x);
+    }
+    
+    deallocate(&root);
+
+    return 0;
+}
